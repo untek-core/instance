cde5ccd518c0e5cc2fd355a5cac6f1a36b6aff8b
vitaliy <theyamshikov@yandex.ru>
2023-12-15 16:10:55 +0600

    upd

--- src/Helpers/MappingHelper.php
+++ src/Helpers/MappingHelper.php
@@ -11,6 +11,7 @@ class MappingHelper
 
     public static function restoreObject(array $data, string $className): object
     {
+
         $object = new $className();
         foreach ($data as $fieldName => $fieldValue) {
             if (!is_scalar($fieldValue)) {
